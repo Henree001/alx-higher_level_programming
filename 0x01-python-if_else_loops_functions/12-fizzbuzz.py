3dfb3263219dd5e602b4abf5dca8f6850db59105
Henree001 <okaforhenry779@gmail.com>
2022-12-11 23:33:40 +0000

    tasks

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
     for i in range(1, 101):
 	    if i % 3 == 0 and i % 5 == 0:
-		    i = FizzBuzz
+		    i = "FizzBuzz"
 	    if i == 3:
-		    i = Fizz
+		    i = "Fizz"
 	    if i == 5:
-		    i = Buzz
+		    i = "Buzz"
 	    if i == 100:
 		    print(f"{i}")
 	    else:
