d9f22a6d953c046b7d9f8d9825fd172a3aff2453
Henree001 <okaforhenry779@gmail.com>
2022-12-13 13:18:41 +0000

    tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     import sys
 
     count = len(sys.argv) - 1
-    if count == 0
-	print("0 arguments.")
+    if count == 0:
+        print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
