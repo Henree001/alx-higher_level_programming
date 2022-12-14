d615feef7092ecfa2d05bc45af5429ded7424234
Henree001 <okaforhenry779@gmail.com>
2022-12-14 16:22:50 +0000

    tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        sentence = None
+        return (0, None)
     length = len(sentence)
     first = sentence[0]
     return (length, first)
