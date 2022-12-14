d254933183a9c909eb0181c7131613e74f1a5dc3
Henree001 <okaforhenry779@gmail.com>
2022-12-14 14:52:09 +0000

    tasks

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    newstring = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            newstring += i
+    return newstring
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            endspace = " "
+            if j == len(matrix[i]) - 1:
+                endspace = ""
+            print("{}".format(matrix[i][j]), end=endspace)
+        print()
