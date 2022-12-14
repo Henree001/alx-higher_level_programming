7010334a02f9d73e79e582714a768a789c852d23
Henree001 <okaforhenry779@gmail.com>
2022-12-14 21:20:51 +0000

    tasks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -17,8 +17,6 @@ int is_palindrome(listint_t **head)
 	}
 	while (k < (i / 2))
 	{
-		if ((i % 2) == 0 && temp2->n != arr[i - 1])
-			return (0);
 		if (temp2->n == arr[i - 1])
 		{
 			temp2 = temp2->next;
