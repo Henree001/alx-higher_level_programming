6dd0a4e40b686e917fe1341e0df86d817c096f08
Henree001 <okaforhenry779@gmail.com>
2022-12-16 21:31:11 +0000

    tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,9 @@
 def best_score(a_dictionary):
     if not a_dictionary or len(a_dictionary) == 0:
         return None
-    keyholder = None
     for key in a_dictionary:
         maxscore = a_dictionary[key]
+        keyholder = key
         break
     for key in a_dictionary:
         if a_dictionary[key] > maxscore:
