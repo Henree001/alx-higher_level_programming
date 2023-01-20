83fd5e0ebd6d1b8aa80d3342ef9a30d898a33a6f
Henree001 <okaforhenry779@gmail.com>
2023-01-20 19:33:33 +0000

    tasks

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,9 +12,9 @@ def print_square(size):
         TypeError: if size is a float and less than 0.
     """
 
-    if isinstance (size, float) and size < 0:
+    if isinstance(size, float) and size < 0:
         raise TypeError('size must be an integer')
-    if not isinstance (size,int):
+    if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
