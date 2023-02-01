0c110e8acdb571366df225520e94ad8fb3fde48f
Henree001 <okaforhenry779@gmail.com>
2023-02-01 21:59:32 +0000

    tasks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import sys
 """Add all arguments to a Python list and save them to a file."""
 
 
-import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
