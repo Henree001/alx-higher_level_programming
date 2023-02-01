c7e090ff656b139c58289bb03438e48404073fa4
Henree001 <okaforhenry779@gmail.com>
2023-02-01 21:37:59 +0000

    tasks

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defines a class Student."""
-import json
 
 
 class Student:
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Python script that adds all arguments to a Python list,
-and then save them to a file:"""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 
