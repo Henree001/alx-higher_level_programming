3a4472506e48be3081e1adb7f70bc26ceebfaa58
Henree001 <okaforhenry779@gmail.com>
2023-02-03 22:45:15 +0000

    tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a base model class."""
+import json
 
 
 class Base:
@@ -16,3 +17,62 @@ class Base:
         else:
             Base. __nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries
+        Args:
+            list_dictionaries: a list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
+            json_string = json.dumps(list_dictionaries)
+            return json_string
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file
+        Args:
+            list_objs: list of instances who inherits of Base class
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+        if list_objs is None:
+            jsonfile.write("[]")
+        else:
+            dict_list = []
+            for items in list_objs:
+                dict_list.append(items.to_dictionary())
+            jsonfile.write(cls.to_json_string(dict_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation
+        Args:
+            json_string: a string representing a list of dictionaries
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantied from a dictionary of attributes."""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                dummy = cls(1, 1)
+            else:
+                dummy = cls(1)
+            dummy.update(**dictionary)
+            return dummy
+
+    @classmetod
+    def load_from_file(cls):
+        """Returns a list of instances"""
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
+            return []
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new square
+        Args:
+            size: The size of the Square
+            x (int): The x coordinate of the new square
+            y (int): The y coordinate of the new square
+            id (int): The identity of the new square
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the Square
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if len(args) == 0:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    self.id = v
+                if k == 'width':
+                    self.__width = v
+                if k == 'height':
+                    self.__height = v
+                if k == 'x':
+                    self.__x = v
+                if k == 'y':
+                    self.__y = v
+        else:
+            try:
+                self.id = args[0]
+                self.__width = args[1]
+                self.__height = args[2]
+                self.__x = args[3]
+                self.__y = args[4]
+            except IndexError:
+                pass
+     def to_dictionary(self):
+        """Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
+      def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
