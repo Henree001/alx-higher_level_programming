aaee8fcf2374b2d2aeb5bec7f0e5206e4d2054e6
Henree001 <okaforhenry779@gmail.com>
2023-02-16 15:16:47 +0000

    tasks

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,2 @@
 --lists all records of the table second_table
-SELECT score, name FROM second_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 --creates a table second_table in the database and add multiples rows
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT));
+CREATE TABLE second_table (id INT, name VARCHAR(256), score INT));
 INSERT INTO second_table (id, name, score)
 VALUES (1, "John", 10),
 (2, "Alex", 13),
