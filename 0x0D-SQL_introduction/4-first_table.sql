ba8da6e1220422608643572dee079dd157623f45
Henree001 <okaforhenry779@gmail.com>
2023-02-16 15:34:01 +0000

    tasks

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 --a script that creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE first_table (id INT, name VARCHAR(256));
