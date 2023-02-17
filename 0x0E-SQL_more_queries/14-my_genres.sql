945d5b95d920c1afcab307f9a6c48665494f551a
Henree001 <okaforhenry779@gmail.com>
2023-02-17 21:05:10 +0000

    tasks

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -8,7 +8,7 @@
 SELECT tv_genres.name
 FROM tv_show_genres
 INNER JOIN tv_shows
-ON tv_shows.id = tv_show_genres.genre_id
+ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.title = "Dexter"
