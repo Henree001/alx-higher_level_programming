878f128c09abff7e0f8c7ed7ccadcaf7fa54e4c2
Henree001 <okaforhenry779@gmail.com>
2023-02-17 20:59:34 +0000

    tasks

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,7 +6,7 @@
 -- The database name will be passed as an argument of the mysql command
 
 SELECT tv_genres.name
-FROM tv_genres
+FROM tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.genre_id
 INNER JOIN tv_genres
