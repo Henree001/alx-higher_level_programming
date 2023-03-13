dbedc01a7f303bc65855f41758fa8b27496b4e36
Henree001 <okaforhenry779@gmail.com>
2023-03-14 00:15:44 +0100

    tasks

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const C = 'C is fun';
-const P = 'Python is cool';
-const J = 'Javascript is amazing';
-console.log(C);
-console.log(P);
-console.log(J);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
