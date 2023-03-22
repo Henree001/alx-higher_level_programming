cc1442951bc2b17c0cd1de5e2efc7cbc33af372d
Henree001 <okaforhenry779@gmail.com>
2023-03-22 23:39:43 +0100

    fix tasks 1 & 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY states.id",
-                .format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY states.id".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
