24d856841ffbec22a14a0ff70833b810e9d9c77b
Henree001 <okaforhenry779@gmail.com>
2023-03-22 23:37:19 +0100

    fix tasks 1 & 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument."""
+"""
+Takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
 
 
 import MySQLdb
@@ -10,7 +12,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = {] ORDER BY states.id",
+    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY states.id",
                 .format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
