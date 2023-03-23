701583ebe6002efb2ab9d6f70f72b276f36c7fc5
Henree001 <okaforhenry779@gmail.com>
2023-03-23 01:27:13 +0100

    fix tasks 13

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,13 +7,13 @@ from the database `hbtn_0e_6_usa`.
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                     sys.argv[1], sys.argv[2], sys.argv[3]))
-    with Session(engine) as session:
+    Session = sessionmaker(engine)
+    with Session() as session:
         txt = select(State).where(State.id == '2')
         row = session.scalars(txt).first()
         row.name = "New Mexico"
         session.commit()
-        session.close()
