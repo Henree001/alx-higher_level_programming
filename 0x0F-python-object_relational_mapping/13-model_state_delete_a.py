6476be3c4e091c3204a0094724bcf0a9d8f36e8e
Henree001 <okaforhenry779@gmail.com>
2023-03-23 12:22:24 +0100

    fix tasks 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                     sys.argv[1], sys.argv[2], sys.argv[3]))
     with Session(engine) as session:
-        txt = select(State).where(State.name.contains('a'))
+        txt = select(State).filter(State.name.contains('a'))
         rows = session.scalars(txt).all()
         for row in rows:
             session.delete(row)
