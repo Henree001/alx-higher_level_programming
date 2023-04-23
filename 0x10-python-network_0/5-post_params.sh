bfde6304aa029310880d90a6ef182a26cfde25a0
Henree001 <okaforhenry779@gmail.com>
2023-04-23 17:01:10 +0100

    fixed task 3 and 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a post request with two headers
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
