d4522f13ccaed49b48b2ce0337e0302361a11791
Henree001 <okaforhenry779@gmail.com>
2023-04-23 17:14:19 +0100

    fixed task 3 and 5

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sets a header var with a value of 98
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sHX GET "X-HolbertonSchool-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a post request with two headers
-curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
