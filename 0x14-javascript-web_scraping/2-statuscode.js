256214587fec19dfb3e778c9e566dfcd3643fd1b
Henree001 <okaforhenry779@gmail.com>
2023-05-31 17:06:12 +0100

    tasks

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(process.argv[2], function (response) {
+
+request(process.argv[2], function (eror, response, body) {
   console.log('code:', response.statusCode); // Print the response status code if a response was received
 });
